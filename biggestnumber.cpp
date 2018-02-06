ab7197e476594378ff9524bbaae261aac78e5ed0
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-02-06 11:19:50 -0800

    added the sorting mech, to sort 2 numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,4 +1,8 @@
-//Author:
+/*Author:Matthew Scheer
+Program: Biggest number
+added code to determine what the biggest number is
+2/6/18
+*/
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
@@ -7,10 +11,20 @@ int main()
 {
   
   cout<<"Please enter a whole number:\n";
-  
+  int n1;
+  cin>>n1;
   cout<<"Please enter another whole number:\n";
+ int n2;
+ cin>>n2;
 
-  cout<<"Of those two numbers, the biggest is: ";
+ if(n1>=n2)
+    {
+  cout<<"Of those two numbers, the biggest is: " << n1<< endl;
+    }
+    else
+    {
+    cout<<"of those two numbers, the biggest is:" << n2<< endl;
+    }
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
